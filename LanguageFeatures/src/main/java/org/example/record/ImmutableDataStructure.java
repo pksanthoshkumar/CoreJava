5aa6dc7d7a7d9c5122f9916c23c939351fad2870
Santhosh Krishnan <pksanthoshkumar@gmail.com>
2024-10-04 19:30:19 -0400

    Record class

---
+++ LanguageFeatures/src/main/java/org/example/record/ImmutableDataStructure.java
@@ -0,0 +1,20 @@
+package org.example.record;
+
+public class ImmutableDataStructure {
+    /*
+    You need to model a Person entity in your application with fields like name, age, and email.
+    The entity should be immutable, and you should provide methods to retrieve each field.
+    Use Java 17 Records to achieve this in a concise way.
+     */
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Santhosh", 60, "santhosh@email.com");
+
+        System.out.println(person1.email());
+        System.out.println(person1.name());
+        System.out.println(person1.age());
+    }
+
+}
+
+record Person (String name, int age, String email) {}
